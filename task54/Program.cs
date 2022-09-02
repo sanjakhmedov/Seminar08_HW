f57ef820a0923ed72b6eeca665fe91ee398d5d63
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-09-03 02:57:09 +0500

    gitignore edited

--- task54/Program.cs
+++ task54/Program.cs
@@ -9,3 +9,55 @@
 // 9 5 3 2
 // 8 4 4 2
 
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matr = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+
+            if (j < matr.GetLength(1) - 1) Console.Write($"{matr[i, j],4}, ");
+            else Console.Write($"{matr[i, j],4} ");
+
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] matrix = CreateMatrixRndInt(4, 4, 0, 9);
+PrintMatrix(matrix);
+
+void DescendingRow(int[,] matr)
+{
+    int temp = matr[0, 0];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 1; j < matr.GetLength(1); j++)
+        {
+            if (matr[i, j] > temp)
+            {
+                
+            }
+        }
+    }
+}
+
+DescendingRow(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
